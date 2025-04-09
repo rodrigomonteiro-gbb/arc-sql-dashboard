3fa748a1e829adc5c098d2f00234e8a5f6421c96
Rodrigo Monteiro <romontei@microsoft.com>
2025-04-09 14:50:16 -0400

    fix execution

--- samples/manage/azure-hybrid-benefit/modify-license-type/create-runbook.ps1
+++ samples/manage/azure-hybrid-benefit/modify-license-type/create-runbook.ps1
@@ -140,7 +140,7 @@ if ($null -eq $principalId) {
         $roleName = $assignment.RoleName
         Write-Host "Assigning role '$roleName' to Managed Identity '$AutomationAccountName' at scope '$Scope'..." -ForegroundColor Yellow
         try {
-            New-AzRoleAssignment -ObjectId $principalId -RoleDefinitionName $roleName -Scope "/subscriptions/$($context.Subscription.SubscriptionId)"   -ErrorAction Stop  | Out-Null
+            New-AzRoleAssignment -ObjectId $principalId -RoleDefinitionName $roleName -Scope "/subscriptions/$($context.Subscription.Id)"   -ErrorAction Stop  | Out-Null
             Write-Host "Role '$roleName' assigned successfully." -ForegroundColor Green
         }
         catch {
