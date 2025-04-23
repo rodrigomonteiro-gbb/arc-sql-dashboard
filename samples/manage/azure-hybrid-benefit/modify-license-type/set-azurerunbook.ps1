b1fff22fa6593681eff386158abfe1f970d90c62
Rodrigo Monteiro <romontei@microsoft.com>
2025-04-23 16:53:34 -0400

    fixx errors

--- samples/manage/azure-hybrid-benefit/modify-license-type/set-azurerunbook.ps1
+++ samples/manage/azure-hybrid-benefit/modify-license-type/set-azurerunbook.ps1
@@ -109,10 +109,9 @@ function Connect-Azure {
 # Connect to Azure.
 Write-Output "Connecting to Azure..."
 Connect-Azure
+$context = Get-AzContext -ErrorAction Stop
 if ($null -ne $targetSubscription) {
     $context = Set-AzContext -Subscription  $targetSubscription -ErrorAction Stop
-}else{
-    $context = Get-AzContext -ErrorAction Stop
 }
 
 # Check if the resource group exists; if not, create it.
